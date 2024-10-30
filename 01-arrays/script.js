e1dab600c3e1f60a283339f01bf89a279293868a
Orsouene <orsoueneelaouizeb@gmail.com>
2024-10-30 14:59:17 +0100

    la 5 domanda in arrays

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -13,12 +13,11 @@ console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = null;
-
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = null;
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
-
+teachers.push("vanessa");
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
 // 7. Verifica se 'Fabio' è presente nell'array teachers
