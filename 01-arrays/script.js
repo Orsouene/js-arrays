6df2255efc42810fa2dae8cf874cd4c818444caa
Orsouene <orsoueneelaouizeb@gmail.com>
2024-10-30 14:16:23 +0100

    La prima domanda  in arrays .

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -1,16 +1,10 @@
-const teachers = [
-  'Nathan',
-  'Ed',
-  'Fabio',
-  'Phil',
-  'Carlo',
-  'Lewis',
-  'Luca'
-]; // NON MODIFICARE QUESTA VARIABILE
+const teachers = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"]; // NON MODIFICARE QUESTA VARIABILE
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
 const fourthTeacher = null;
+teachers.splice(3, 0, fourthTeacher);
+console.log(teachers.indexOf(fourthTeacher));
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
