3a2bdfe620255eb9800c5d50907f31b2f9c60d53
Orsouene <orsoueneelaouizeb@gmail.com>
2024-10-30 15:28:21 +0100

    la 3 domanda in arrays

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -12,14 +12,17 @@ teachers.splice(4, 1, "Patrick");
 console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop();
+console.log(lastTeacher);
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = null;
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 teachers.push("vanessa");
+console.log(teachers);
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 teachers.unshift("sarah");
+console.log(teachers);
 // 7. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
 const isFabioPresent = null;
